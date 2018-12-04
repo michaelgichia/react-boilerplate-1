f26d583a68a93cd163cfb615b2743cbe893bd3dc
michael gichia <gichuru.gichia@gmail.com>
2018-12-04 17:35:42 +0300

    :sparkles: Removed redux-saga

--- app/configureStore.js
+++ app/configureStore.js
@@ -5,16 +5,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'connected-react-router/immutable';
-import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
-
-const sagaMiddleware = createSagaMiddleware();
 
 export default function configureStore(initialState = {}, history) {
   // Create the store with two middlewares
-  // 1. sagaMiddleware: Makes redux-sagas work
-  // 2. routerMiddleware: Syncs the location/URL path to the state
-  const middlewares = [sagaMiddleware, routerMiddleware(history)];
+  // 1. routerMiddleware: Syncs the location/URL path to the state
+  const middlewares = [routerMiddleware(history)];
 
   const enhancers = [applyMiddleware(...middlewares)];
 
@@ -35,9 +31,7 @@ export default function configureStore(initialState = {}, history) {
   );
 
   // Extensions
-  store.runSaga = sagaMiddleware.run;
   store.injectedReducers = {}; // Reducer registry
-  store.injectedSagas = {}; // Saga registry
 
   // Make reducers hot reloadable, see http://mxs.is/googmo
   /* istanbul ignore next */
